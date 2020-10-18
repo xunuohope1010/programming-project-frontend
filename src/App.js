e18801ab9d1f475e91e3b58937343abd5cfb5bf2
NUO XU <nuox@usc.edu>
2020-10-17 19:58:33 -0700

    react frontend

--- src/App.js
+++ src/App.js
@@ -7,110 +7,122 @@ import LoginComponent from "./components/login.component";
 import SignupComponent from "./components/signup.component";
 import ProfileComponent from "./components/profile.component";
 import ClassificationComponent from "./components/classification.component";
+import IdleTimer from 'react-idle-timer'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentUser: null,
-      redirect:false
-    };
-  }
-
-  componentDidMount() {
-    this.setState({redirect:false})
-    const cookie = JSON.parse(localStorage.getItem('cookie'));
-    if (cookie) {
-      this.setState({
-        currentUser: cookie,
-      })
-      setTimeout(()=> {
-        localStorage.clear();
-      }, 120000)
+    constructor(props) {
+        super(props);
+        this.idleTimer = null
+        this.state = {
+            currentUser: null,
+            redirect: false,
+        };
     }
-  }
 
-  logOut = () => {
-    localStorage.removeItem("cookie");
-  }
+    componentDidMount() {
+        this.setState({redirect: false})
+        const cookie = JSON.parse(localStorage.getItem('cookie'));
+        if (cookie) {
+            this.setState({
+                currentUser: cookie,
+            })
+        }
+    }
 
+    logOut = () => {
+        localStorage.removeItem("cookie");
+    }
 
-  render() {
+    handleOnIdle = () => {
+        localStorage.clear()
+    }
 
-    const { currentUser} = this.state;
-    return (
-        <div>
-          <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <Link to={"/"} className="navbar-brand">
-              Interview
-            </Link>
-            <div className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to={"/home"} className="nav-link">
-                  Home
-                </Link>
-              </li>
+    render() {
 
-              {currentUser && (
-                  <li className="nav-item">
-                    <Link to={"/profile"} className="nav-link">
-                      Profile
+        const {currentUser} = this.state;
+        return (
+            <div>
+                <IdleTimer
+                    ref={ref => {
+                        this.idleTimer = ref
+                    }}
+                    timeout={1000 * 120}
+                    // onActive={this.handleOnActive}
+                    onIdle={this.handleOnIdle}
+                    // onAction={this.handleOnAction}
+                    debounce={250}
+                />
+                <nav className="navbar navbar-expand navbar-dark bg-dark">
+                    <Link to={"/"} className="navbar-brand">
+                        Interview
                     </Link>
-                  </li>
-              )}
+                    <div className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to={"/home"} className="nav-link">
+                                Home
+                            </Link>
+                        </li>
 
-              {currentUser && (
-                  <li className="nav-item">
-                    <Link to={"/classification"} className="nav-link">
-                      Classification
-                    </Link>
-                  </li>
-              )}
+                        {currentUser && (
+                            <li className="nav-item">
+                                <Link to={"/profile"} className="nav-link">
+                                    Profile
+                                </Link>
+                            </li>
+                        )}
 
+                        {currentUser && (
+                            <li className="nav-item">
+                                <Link to={"/classification"} className="nav-link">
+                                    Classification
+                                </Link>
+                            </li>
+                        )}
+
+                    </div>
+
+                    {currentUser ? (
+                        <div className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to={"/profile"} className="nav-link">
+                                    {currentUser.username}
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/login" className="nav-link" onClick={() => this.logOut()}>
+                                    LogOut
+                                </a>
+                            </li>
+                        </div>
+                    ) : (
+                        <div className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to={"/login"} className="nav-link">
+                                    Login
+                                </Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link to={"/signup"} className="nav-link">
+                                    Sign Up
+                                </Link>
+                            </li>
+                        </div>
+                    )}
+                </nav>
+
+                <div className="container mt-3">
+                    <Switch>
+                        <Route exact path={["/", "/home"]} component={Home}/>
+                        <Route exact path="/login" component={LoginComponent}/>
+                        <Route exact path="/signup" component={SignupComponent}/>
+                        <Route exact path="/profile" component={ProfileComponent}/>
+                        <Route exact path="/classification" component={ClassificationComponent}/>
+                    </Switch>
+                </div>
             </div>
-
-            {currentUser ? (
-                <div className="navbar-nav ml-auto">
-                  <li className="nav-item">
-                    <Link to={"/profile"} className="nav-link">
-                      {currentUser.username}
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <a href="/login" className="nav-link" onClick={() => this.logOut()}>
-                      LogOut
-                    </a>
-                  </li>
-                </div>
-            ) : (
-                <div className="navbar-nav ml-auto">
-                  <li className="nav-item">
-                    <Link to={"/login"} className="nav-link">
-                      Login
-                    </Link>
-                  </li>
-
-                  <li className="nav-item">
-                    <Link to={"/signup"} className="nav-link">
-                      Sign Up
-                    </Link>
-                  </li>
-                </div>
-            )}
-          </nav>
-
-          <div className="container mt-3">
-            <Switch>
-              <Route exact path={["/", "/home"]} component={Home}/>
-              <Route exact path="/login" component={LoginComponent}/>
-              <Route exact path="/signup" component={SignupComponent}/>
-              <Route exact path="/profile" component={ProfileComponent}/>
-              <Route exact path="/classification" component={ClassificationComponent}/>
-            </Switch>
-          </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
